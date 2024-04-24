83f360d5f1638e9f6a2ba148f1103bb4d98648ee
Florian Pieles <126450255+flom3h@users.noreply.github.com>
2024-04-24 14:42:51 +0200

    Add project files.

---
+++ src/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kalender_Project_FlorianRohat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
